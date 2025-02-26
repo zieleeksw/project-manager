b259282c6287f938976d52d76e3e67a7d089d1a4
Szymon Zieliński <szielinski1508@wp.pl>
2025-02-26 22:41:56 +0100

    [SERVER] Spring boot application initialization

---
+++ project-manager-server/src/test/java/pl/zieleeksw/project_manager/ProjectManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.zieleeksw.project_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
